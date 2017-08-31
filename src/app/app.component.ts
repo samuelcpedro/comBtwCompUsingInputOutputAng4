8b6058aa556491b6215607953c5048ad0c04bf70
Samuel Pedro <samuel.pedro@pmasolutions.com>
2017-08-31 15:33:59 +0000

    minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,31 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title: string = 'Component Communications';
+	
 	employeeRecords: any = [
 		{ 
 			eNameValueFromAppComp: 'A', 
-			eCityValueFromAppComp: 'City_1',
+			eCityValueFromAppComp: 'City_1', 
 			eSalaryValueFromAppComp: 50000 
 		},
-		{ eNameValueFromAppComp: 'B', eCityValueFromAppComp: 'City_2', eSalaryValueFromAppComp: 60000 },
-		{ eNameValueFromAppComp: 'C', eCityValueFromAppComp: 'City_3', eSalaryValueFromAppComp: 70000 },
-		{ eNameValueFromAppComp: 'D', eCityValueFromAppComp: 'City_4', eSalaryValueFromAppComp: 80000 },
+		{ 
+			eNameValueFromAppComp: 'B', 
+			eCityValueFromAppComp: 'City_2', 
+			eSalaryValueFromAppComp: 60000 
+		},
+		{ 
+			eNameValueFromAppComp: 'C', 
+			eCityValueFromAppComp: 'City_3', 
+			eSalaryValueFromAppComp: 70000 
+		},
+		{ 
+			eNameValueFromAppComp: 'D', 
+			eCityValueFromAppComp: 'City_4', 
+			eSalaryValueFromAppComp: 80000 
+		},
 	]
 
 	selectedEmployeeData: any = {
-		selectedName:'',
-		selectedCity:'',
-		selectedSalary:''
+		selectedName:	'',
+		selectedCity:	'',
+		selectedSalary:	''
 	};
 	
 	
 	getRecord(data: any): void{
 		this.selectedEmployeeData = data;
 	}
-	
-	/*
-	getRecord(): void{
-		console.log('SendRecord Event emitted');
-	}
-	*/
 }
