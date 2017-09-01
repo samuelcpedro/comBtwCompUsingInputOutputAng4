037d278b7469c5a380504ccf9743134bb9a4bc90
Samuel Pedro <samuel.pedro@pmasolutions.com>
2017-09-01 11:40:21 +0000

    Input Output between 2 comp v2

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-child',
-  templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+	selector: 'app-child',
+	//templateUrl: './child.component.html',
+	template:`
+		<h2>Child Tutorials Component</h2>
+		<label>Enter Child ChildComponent Value</label>
+		<input type="text" #childtext (keyup)="onChange(childtext.value)">
+		<p>Value from Parent ParentComponent is </p>
+		{{parentData}}
+	`,
+	styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent {
 
-  constructor() { }
+	@Input() parentData: string;
+	@Output() childEvent = new EventEmitter();
 
-  ngOnInit() {
-  }
+	onChange(value:string){
+		this.childEvent.emit(value);
+	}
 
 }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-parent',
-  templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.css']
+	selector: 'app-parent',
+	//templateUrl: './parent.component.html',
+	template: `
+		<h1>Hello World</h1>
+		<h2>Parent App Component</h2>
+		<label for="">Enter Parent AppComponent Value</label>
+		<input type="text" #ptext (keyup)="0">
+		<!-- (keyup)="0" dummie event that refresh Page -->
+		<!-- Template variable que faz referencia ao elemento input -->
+		<p>Value from Child TutorialsComponent is </p>
+		{{ childData }}
+		<app-child [parentData]="ptext.value" (childEvent)="childData=$event"></app-child>
+	`,
+	styleUrls: ['./parent.component.css']
 })
-export class ParentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class ParentComponent {
 
 }
